789a27170411c393fb8bea38944fcbbb147feb7a
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-02 13:00:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
-
+ array = "Hello there, and how are you?"
  
 def reverse_each_word(array)
   puts "Hello there, and how are you?" 
- array = "Hello there, and how are you?" 
- new_string = reverse.array
+  new_string = reverse.array
 end 
  
  
