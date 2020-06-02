190f831e1a85cb5ae916ebe1e25ae2cdb7d97471
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-02 12:58:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,14 @@ def reverse_each_word(array)
   puts "Hello there, and how are you?" 
  array = "Hello there, and how are you?" 
  new_string = reverse.array
- this array is what we want to reverse 
+ 
+ 
  
 
 end 
+puts "Enter the String:"
+str1=gets.chomp
+
+newstr=str1.reverse
+
+puts "The reverse of #{str1} is #{newstr}"
