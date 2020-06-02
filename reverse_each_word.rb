f10a7dce9ded4ac100a287bf6e28dc2b7bbaa174
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-02 12:06:57 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+string = "Hello there, and how are you?"
+
+end 
+text = "Hello there, and how are you?"
+puts "text: #{text}"
+
+wordsReversed = text.split(" ").reverse.join(" ")
+
+puts "Words reversed: #{wordsReversed}"
