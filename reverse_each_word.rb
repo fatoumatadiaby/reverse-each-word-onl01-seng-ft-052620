4bb9d691972d09a0f1880b90a1cd8e57a30fb678
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-02 12:14:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string)
-string = "Hello there, and how are you?"
+def reverse_each_word(array)
+array = "Hello there, and how are you?"
 end 
 text = "Hello there, and how are you?"
 puts "text: #{text}"
